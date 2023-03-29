fd7c91f10cd4b0c2335cad1c655b0899baeb1b80
Milan-Simform <milan.c@simformsolutions.com>
2023-03-29 16:33:58 +0530

    1 - files updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Git With Android Studio HHHHIIIIIInjnjnI",
+      title: "Git With Android Studio Pro Bro",
       theme: ThemeData(useMaterial3: true),
       home: const Home(),
     );
